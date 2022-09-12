8f4a5359b53b573417ff7d6b19d764292b965568
Matt Pannella <mattpannella@gmail.com>
2022-09-11 23:47:05 -0400

    fixed checkbox clicking

--- CoreSelector.Designer.cs
+++ CoreSelector.Designer.cs
@@ -35,20 +35,19 @@
             // 
             // coresList
             // 
+            this.coresList.CheckOnClick = true;
             this.coresList.FormattingEnabled = true;
-            this.coresList.Location = new System.Drawing.Point(11, 22);
-            this.coresList.Margin = new System.Windows.Forms.Padding(2);
+            this.coresList.Location = new System.Drawing.Point(19, 36);
             this.coresList.Name = "coresList";
-            this.coresList.Size = new System.Drawing.Size(456, 508);
+            this.coresList.Size = new System.Drawing.Size(772, 796);
             this.coresList.TabIndex = 0;
             // 
             // Button_Save
             // 
             this.Button_Save.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Button_Save.Location = new System.Drawing.Point(183, 551);
-            this.Button_Save.Margin = new System.Windows.Forms.Padding(2);
+            this.Button_Save.Location = new System.Drawing.Point(311, 904);
             this.Button_Save.Name = "Button_Save";
-            this.Button_Save.Size = new System.Drawing.Size(111, 35);
+            this.Button_Save.Size = new System.Drawing.Size(189, 57);
             this.Button_Save.TabIndex = 1;
             this.Button_Save.Text = "Save";
             this.Button_Save.UseVisualStyleBackColor = true;
@@ -56,14 +55,13 @@
             // 
             // CoreSelector
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 612);
+            this.ClientSize = new System.Drawing.Size(860, 1004);
             this.Controls.Add(this.Button_Save);
             this.Controls.Add(this.coresList);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "CoreSelector";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
