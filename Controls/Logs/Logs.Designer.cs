1598b5e4e818fe0d778008ae23b13940ce08badd
RetroDriven.com <44145655+RetroDriven@users.noreply.github.com>
2023-02-23 12:31:46 -0500

    Added Logs as it was being ignored

---
+++ Controls/Logs/Logs.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Pocket_Updater.Controls.Logs
+{
+    partial class Logs
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            this.Panel_Top = new Guna.UI2.WinForms.Guna2Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.guna2Separator2 = new Guna.UI2.WinForms.Guna2Separator();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Clear = new Guna.UI2.WinForms.Guna2Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.Panel_Top.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel_Top
+            // 
+            this.Panel_Top.AutoSize = true;
+            this.Panel_Top.Controls.Add(this.label4);
+            this.Panel_Top.Controls.Add(this.guna2Separator2);
+            this.Panel_Top.CustomizableEdges = customizableEdges1;
+            this.Panel_Top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel_Top.Location = new System.Drawing.Point(0, 0);
+            this.Panel_Top.Name = "Panel_Top";
+            this.Panel_Top.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            this.Panel_Top.Size = new System.Drawing.Size(622, 49);
+            this.Panel_Top.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 28);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Logs";
+            // 
+            // guna2Separator2
+            // 
+            this.guna2Separator2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(107)))), ((int)(((byte)(121)))));
+            this.guna2Separator2.Location = new System.Drawing.Point(4, 34);
+            this.guna2Separator2.Name = "guna2Separator2";
+            this.guna2Separator2.Size = new System.Drawing.Size(250, 12);
+            this.guna2Separator2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 538);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 15, 0, 15);
+            this.panel1.Size = new System.Drawing.Size(622, 70);
+            this.panel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.Clear, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 15);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(622, 40);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // Clear
+            // 
+            this.Clear.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Clear.BorderRadius = 5;
+            this.Clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Clear.CustomizableEdges = customizableEdges3;
+            this.Clear.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Clear.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Clear.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Clear.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Clear.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Clear.ForeColor = System.Drawing.Color.White;
+            this.Clear.Location = new System.Drawing.Point(253, 3);
+            this.Clear.Name = "Clear";
+            this.Clear.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            this.Clear.Size = new System.Drawing.Size(116, 34);
+            this.Clear.TabIndex = 0;
+            this.Clear.Text = "Clear Logs";
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.panel2.Size = new System.Drawing.Size(622, 489);
+            this.panel2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoSize = true;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(44)))), ((int)(((byte)(66)))));
+            this.textBox1.BorderRadius = 5;
+            this.textBox1.BorderThickness = 0;
+            this.textBox1.CustomizableEdges = customizableEdges5;
+            this.textBox1.DefaultText = "";
+            this.textBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.textBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.textBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(44)))), ((int)(((byte)(66)))));
+            this.textBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textBox1.Location = new System.Drawing.Point(2, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '\0';
+            this.textBox1.PlaceholderText = "";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.SelectedText = "";
+            this.textBox1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            this.textBox1.Size = new System.Drawing.Size(618, 489);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Logs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(62)))), ((int)(((byte)(78)))));
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Panel_Top);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Logs";
+            this.Size = new System.Drawing.Size(622, 608);
+            this.Panel_Top.ResumeLayout(false);
+            this.Panel_Top.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel Panel_Top;
+        private Label label4;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator2;
+        private Panel panel1;
+        private Panel panel2;
+        public Guna.UI2.WinForms.Guna2TextBox textBox1;
+        private TableLayoutPanel tableLayoutPanel1;
+        public Guna.UI2.WinForms.Guna2Button Clear;
+    }
+}
---
+++ Controls/Logs/Logs.cs
@@ -0,0 +1,69 @@
+﻿using Pocket_Updater.Forms.Message_Box;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pocket_Updater.Controls.Logs
+{
+    public partial class Logs : UserControl
+    {
+        public Logs()
+        {
+            InitializeComponent();
+
+            string Current_Dir = Directory.GetCurrentDirectory();
+            string LogFile = Current_Dir + "\\Pocket_Updater_Log.txt";
+
+            if (File.Exists(LogFile))
+            {
+                textBox1.Text = File.ReadAllText(Current_Dir + "\\Pocket_Updater_Log.txt");
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.ScrollToCaret();
+                textBox1.Refresh();
+                textBox1.Select();
+            }
+            else
+            {
+                textBox1.Text = "No Log File Found!";
+                Clear.Enabled= false;
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            string Current_Dir = Directory.GetCurrentDirectory();
+            string LogFile = Current_Dir + "\\Pocket_Updater_Log.txt";
+
+            if (File.Exists(LogFile))
+            {
+                try
+                {
+                    Clear.Enabled = false;
+                    File.Delete(LogFile);
+                    Message_Box form = new Message_Box();
+                    form.label1.Text = "Log File Cleared!";
+                    form.Show();
+                    textBox1.Text = "No Log File Found!";
+                }
+                catch (Exception ex)
+                {
+                    Message_Box form = new Message_Box();
+                    form.label1.Text = ex.Message;
+                    form.Show();
+                }
+            }
+            else
+            {
+                Message_Box form = new Message_Box();
+                form.label1.Text = "No Log File Found!";
+                form.Show();
+            }
+        }
+    }
+}
